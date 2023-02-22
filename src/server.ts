540c7f7a1f89b7799b7156a74fb61238ef0118ea
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-02-21 20:49:56 -0400

    feat: add post route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 async function bootstrap() {
   const fastify = Fastify({ logger: true });
 
 
-fastify.get('/posts',  () => {
-    
+fastify.get('/posts',  async() => {
+    const posts = await prisma.post.findMany()
 
-    return 'teste'
+    return {posts}
+})
+
+fastify.post('/posts', async() => {
 })
 
   await fastify.listen({port: 3333})
