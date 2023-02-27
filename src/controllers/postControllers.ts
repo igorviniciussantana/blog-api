cb7fc852f81852f006370e8a69ab877fdf37fe18
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-02-27 10:22:18 -0400

    feat: add Delete Post controller

--- src/controllers/postControllers.ts
+++ src/controllers/postControllers.ts
@@ -50,6 +50,52 @@ export async function createPost(
     await prisma.post.create({ data: { title, content, banner_url } });
     return reply.status(201).send(`Post ${title} foi cadastrado com sucesso`);
   } catch (err) {
-    return reply.status(500).send("Não foi possível cadastrar o post" + err);
+    return reply.status(400).send("Não foi possível cadastrar o post" + err);
   }
+}
+
+export async function updatePosts(
+  request: FastifyRequest<{
+    Params: {
+      id: string;
+    };
+  }>,
+  reply: FastifyReply
+) {
+  const createPostBody = z.object({
+    title: z.string(),
+    content: z.string(),
+    banner_url: z.string(),
+  });
+
+  try {
+    const { title, content, banner_url } = createPostBody.parse(request.body);
+    await prisma.post.update({
+      where: { id: request.params.id },
+      data: { title, content, banner_url },
+    });
+
+    return reply.status(201).send(`Post ${title} foi atualizado com sucesso`);
+  } catch (err) {
+    return reply.status(400).send("Não foi possível atualizar o post" + err);
+  }
+}
+
+export async function deletePost(
+  request: FastifyRequest<{
+    Params: {
+      id: string;
+    };
+  }>,
+  reply: FastifyReply
+) {
+
+  try{
+  await prisma.post.delete({ where: { id: request.params.id } });
+
+  return reply.status(201).send("Post deletado com sucesso!");
+}catch(err){
+  return reply.status(400).send("Não foi possível deletar o post" + err);
+
+}
 }
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -4,8 +4,10 @@ import { z } from "zod";
 import { FastifyRequest } from "fastify";
 import {
   createPost,
+  deletePost,
   getPosts,
   getSinglePost,
+  updatePosts,
 } from "../controllers/postControllers";
 
 export async function postRoutes(fastify: FastifyInstance) {
@@ -15,45 +17,7 @@ export async function postRoutes(fastify: FastifyInstance) {
 
   fastify.get("/posts/:id", getSinglePost);
 
-  fastify.put(
-    "/posts/:id",
-    async (
-      request: FastifyRequest<{
-        Params: {
-          id: string;
-        };
-      }>,
-      reply
-    ) => {
-      const createPostBody = z.object({
-        title: z.string(),
-        content: z.string(),
-        banner_url: z.string(),
-      });
+  fastify.put("/posts/:id", updatePosts);
 
-      const { title, content, banner_url } = createPostBody.parse(request.body);
-      await prisma.post.update({
-        where: { id: request.params.id },
-        data: { title, content, banner_url },
-      });
-
-      return reply.status(201).send(`Post ${title} foi atualizado com sucesso`);
-    }
-  );
-
-  fastify.delete(
-    "/posts/:id",
-    async (
-      request: FastifyRequest<{
-        Params: {
-          id: string;
-        };
-      }>,
-      reply
-    ) => {
-      await prisma.post.delete({ where: { id: request.params.id } });
-
-      return reply.status(201).send("Post deletado com sucesso!");
-    }
-  );
+  fastify.delete("/posts/:id", deletePost);
 }
