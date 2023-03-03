f76029d024377fe0aadaf82749ede84677cf6604
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-03-03 14:07:39 -0400

    feat: setup jwt fastify

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
-import { FastifyRequest } from "fastify";
-import { prisma } from "./lib/prisma";
 import { postRoutes } from "./routes/post";
 import { userRoutes } from "./routes/user";
+import jwt from '@fastify/jwt'
 
 async function bootstrap() {
   const fastify = Fastify({ logger: true });
+
+  const jwt_secret = process.env.JWT_SECRET || 'secret';
+  await fastify.register(jwt, {
+    secret: jwt_secret
+  });
 
   await fastify.register(cors, {
     origin: true,
