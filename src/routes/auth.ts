2899a388a8d1e8a4402f1899aa96fd254d4869c7
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-03-03 15:08:14 -0400

    style(auth.ts): auto format using prettier

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,69 +5,59 @@ import { authenticate } from "../plugins/authenticate";
 import { decrypt } from "../services/crypto";
 
 export async function authRoutes(fastify: FastifyInstance) {
-
-    fastify.get(
-        "/me",
-        {
-          onRequest: [authenticate],
+  fastify.get(
+    "/me",
+    {
+      onRequest: [authenticate],
+    },
+    async (request) => {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: request.user.sub,
         },
-        async (request) => {
+      });
 
-const user = await prisma.user.findUnique({
-    where:{
-        id: request.user.sub
+      return { user };
     }
-})
+  );
 
-          return { user };
+  fastify.post("/signin", async (request, reply) => {
+    const createUserBody = z.object({
+      username: z.string(),
+      password: z.string(),
+    });
+
+    const { username, password } = createUserBody.parse(request.body);
+
+    let user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      return reply.status(404).send({
+        message: "Usuário não encontrado.",
+      });
+    }
+
+    if (decrypt(user.password) === password) {
+      const token = fastify.jwt.sign(
+        {
+          name: user.name,
+          username: user.username,
+        },
+        {
+          sub: user.id,
+          expiresIn: "7 days",
         }
       );
 
+      return { token };
+    }
 
-    fastify.post("/signin", async (request, reply) => {
-        const createUserBody = z.object({
-            username: z.string(),
-            password: z.string()
-          });
-    
-    
-          const {username, password} = createUserBody.parse(request.body);
-    
-          let user = await prisma.user.findUnique({
-            where: {
-                username,
-            }
-          })
-    
-          if(!user){
-            return reply.status(404).send({
-                message: 'Usuário não encontrado.'
-            })
-          }
-    
-    
-          if(decrypt(user.password)  === password){
-            
-        const token = fastify.jwt.sign(
-            {
-              name: user.name,
-              username: user.username,
-    
-            },
-            {
-              sub: user.id,
-              expiresIn: "7 days",
-            }
-          );
-    
-          return {token}
-          }
-
-          return reply.status(400).send({
-            message: "Senha incorreta."
-          })
+    return reply.status(400).send({
+      message: "Senha incorreta.",
     });
-
-
-
+  });
 }
